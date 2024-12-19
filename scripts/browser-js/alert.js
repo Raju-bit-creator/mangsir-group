fb27014aff1271c57b32e8307a85caee41e5f928
Raju <rajutamang3456@gmail.com>
2024-12-19 15:48:19 +0545

    varaible declaration

---
+++ scripts/browser-js/alert.js
@@ -0,0 +1,12 @@
+// alert('hello namaster')
+// const conf=confirm('are you sure?')
+// console.log("this is value in boolean:", conf);
+// if (conf==true) {
+//     console.log("friend request accepted");
+    
+// } else {
+//     console.log("friend request rejected");
+// }
+const p= prompt("Enter your favourite number!")
+console.log("your favourite number is:", p);
+
---
+++ scripts/variables.js
@@ -0,0 +1,41 @@
+//variable declaration
+// var x=10
+// var x=20
+
+// console.log("this is sum of x :", x);
+// {
+//     var x=30
+//     console.log("this is block x:", x);
+    
+// }
+// console.log("this is global x:", x)
+
+// let x=10
+// console.log("this is top x", x);
+// {
+//     let x=30
+//     console.log("this is block x",x);
+    
+// }
+// console.log("this is button x", x);
+
+const y=10
+console.log("this is top y", y);
+{
+    const y=30
+    console.log("this is block y",y);
+    
+}
+console.log("this is button y", y);
+
+// let a="apple"
+// let b= 5
+// let c= true
+// let d= 
+// console.log("this is type of a, b , c", typeof a, typeof b, typeof c, typeof d);
+
+
+
+
+
+
