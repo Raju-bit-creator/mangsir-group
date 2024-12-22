f811afe73ded3e650498787d5a4933c8c13fc928
Raju <rajutamang3456@gmail.com>
2024-12-22 15:58:22 +0545

    loop

---
+++ scripts/loops.js
@@ -0,0 +1,52 @@
+// let students=[
+//     {
+//         name: "ramesh",
+//         paidStatus: false
+//     },
+//     {
+//         name: "ganga",
+//         paidStatus: true
+//     },
+//     {
+//         name: "jasmine",
+//         paidStatus: false
+//     }
+// ]
+//for loopfor (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+// }
+   
+// for (let index = 0; index < 100; index++) {
+    
+//     console.log(index);
+    
+    
+// }
+// multiplication table of 2 upto 10
+// let realNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let oddNumber = []
+// let evenNumber = []
+// realNumbers.forEach((i)=>{
+//     if (i%2==0) {
+//         evenNumber.push(i)
+//     } else {
+//         oddNumber.push(i)
+//     }
+// })
+// console.log("this is even numbers array", evenNumber);
+// console.log("this is odd numbers array", oddNumber);
+
+let realNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let oddNumber = []
+let evenNumber = []
+realNumbers.map((i)=>{
+    if (i%2==0) {
+        evenNumber.push(i)
+    } else {
+        oddNumber.push(i)
+    }
+})
+console.log("this is even numbers array", evenNumber);
+console.log("this is odd numbers array", oddNumber);
+console.log(o-100);
